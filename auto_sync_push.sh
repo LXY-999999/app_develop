1a5bd1d754aabe04df0b32c16756eefe9098abc8
LXY-999999 <LXY-999999@users.noreply.github.com>
2026-03-03 15:12:23 +0800

    fix autopush script cd builtin

--- auto_sync_push.sh
+++ auto_sync_push.sh
@@ -6,7 +6,7 @@ export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 SRC_DIR="/Users/luxiaoyue/Documents/lxy/code/tushuare_crawl"
 DST_REPO="/Users/luxiaoyue/Documents/lxy/report_paper"
 
-/bin/cd /
+cd /
 
 setopt null_glob
 for f in $SRC_DIR/AI应用_concept_cons_sme_逐家AI深度分析_AI*.xlsx; do
